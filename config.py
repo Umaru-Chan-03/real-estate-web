131387231d4675351c02403cd1e72fb9ea60f056
Umaru-Chan-03 <phamthanhanh0313@gmail.com>
2026-03-28 12:08:44 +0700

    Add files via upload

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
+import os
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://tupn:123456@127.0.0.1/real_estate"
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL") or "sqlite:///local.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    SECRET_KEY = "super-secret-key"
+    SECRET_KEY = os.getenv("SECRET_KEY", "super-secret-key")
 
-    ADMIN_USERNAME = "admin"
-    ADMIN_PASSWORD = "123456"
+    ADMIN_USERNAME = os.getenv("ADMIN_USERNAME", "admin")
+    ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD", "123456")
